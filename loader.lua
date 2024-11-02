d13b5d947f14c6119df8762589964d2732819fba
Codingninjagiri <fiskerjensenspark@gmail.com>
2024-11-02 16:08:06 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,15 +4,13 @@ local smoll = loadstring(game:HttpGet('https://raw.githubusercontent.com/Codingn
 -- Initialize the UI with a title and size
 local GUI = smoll.SetupUI({
     Title = "Exodus | Loader",
-    Size = UDim2.new(0, 500, 0, 330),  -- Adjusts the size of the UI
+    Size = UDim2.new(0, 500, 0, 330)  -- Adjusts the size of the UI
 })
 
 -- Main Page tab
 local mainTab = GUI.CreateTabs({
     Text = "Main Page"
 })
-
-
 
 -- Main Page Buttons
 mainTab.CreateButtons({
@@ -22,4 +20,4 @@ mainTab.CreateButtons({
     end
 })
 
-
+-- Additional buttons or functionality can be added below
